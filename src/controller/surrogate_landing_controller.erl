5851d5468ea738cfad81a9b5494ac24e8adb084b
Jake Breindel <j.breindel2@outlook.com>
2015-05-20 19:37:17 -0400

    Added routes templates and a login controller.

--- src/controller/surrogate_landing_controller.erl
+++ src/controller/surrogate_landing_controller.erl
@@ -4,4 +4,13 @@
 -compile(export_all).
 
 landing('GET', []) ->
-	{output, "Hello, Dominique!"}.
+	case boss_db:find(config, [{id, 1}], [{limit,1}]) of
+		[Config] ->
+			{redirect, proplists:get_value("redirect", Req:post_params(), "/login"), user:login_cookies()}
+		[] -> 
+			{output, []}
+	end;
+end.
+
+landing('POST', []) ->
+	{output, []}.
--- src/model/user.erl
+++ src/model/user.erl
@@ -14,3 +14,16 @@
 -module(user, [Id, UserName, PasswordHash]).
 -compile(export_all).
 -has({rg_account, one, downloads, many}).
+
+-define(SECRET_STRING, "SecretString!!").
+
+session_identifier() ->
+    mochihex:to_hex(erlang:md5(?SECRET_STRING ++ Id)).
+
+check_password(Password) ->
+    Salt = mochihex:to_hex(erlang:md5(UserName)),
+    user_lib:hash_password(Password, Salt) =:= PasswordHash.
+
+login_cookies() ->
+    [ mochiweb_cookies:cookie("user_id", Id, [{path, "/login"}]),
+        mochiweb_cookies:cookie("session_id", session_identifier(), [{path, "/login"}]) ].
