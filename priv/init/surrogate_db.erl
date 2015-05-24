ef1e0c59cbe0560419c82898f9fc873a374b4af9
Jake Breindel <j.breindel2@outlook.com>
2015-05-24 00:41:53 -0400

    Fixed a bug where we couldn't write to the database.

--- priv/init/surrogate_db.erl
+++ priv/init/surrogate_db.erl
@@ -1,31 +1,45 @@
 -module(surrogate_db).
--export([init/0, migrate/0, rebuild/0, recreate_table/1]).
+-export([init/0, migrate/0, rebuild/0]).
 
 -define(APPNAME, surrogate).
 -define(MODELS, [list_to_atom(M) || M <- boss_files:model_list(?APPNAME)]).
 -define(NODES, [node()]).
 
 init() ->
-    mnesia:stop(),
-    mnesia:create_schema(?NODES),
-    mnesia:change_table_copy_type(schema, node(), disc_copies),
-    mnesia:start(),
-    ExistingTables = mnesia:system_info(tables),
-    TablesToCreate = (?MODELS ++ ['_ids_']) -- ExistingTables,
-    [create_table(T) || T <- TablesToCreate],
-    {ok, []}.
+  init_db (),
+  ok.
 
-create_table('_ids_') ->
-    create_table(?NODES, '_ids_', [type, id, count]);
-create_table(Model) ->
-    io:format("Installing table ~p~n",[Model]),
-    DummyRecord = boss_record_lib:dummy_record(Model),
-    Attributes = DummyRecord:attribute_names(),
-    create_table(?NODES, Model, Attributes).
+init_db () ->
+  init_db ([node ()]). % only for local node? what about nodes()++[node()]?
+init_db (Nodes) ->
+  mnesia:create_schema (Nodes),
+  mnesia:change_table_copy_type (schema, node(), disc_copies), % only for local node?
+  mnesia:start (),
+  ModelList = [ list_to_atom (M) || M <- boss_files:model_list (?APPNAME) ],
+  ExistingTables = mnesia:system_info(tables),
+  Tables = (ModelList ++ ['_ids_']) -- ExistingTables,
+  create_model_tables (Nodes, Tables).
 
-create_table(Nodes, Table, Attributes) ->
-    mnesia:create_table(Table, [{attributes, Attributes},
-                               {disc_copies, Nodes}]).
+% create all the tables
+create_model_tables (_, []) -> ok;
+create_model_tables (Nodes, [Model | Models]) ->
+  [create_model_table (Nodes, Model)] ++
+   create_model_tables (Nodes, Models).
+
+% specific tables (not generated from model)
+create_model_table (Nodes, '_ids_') ->
+  create_table (Nodes, '_ids_', [type, id]);
+
+% tables generated from model
+create_model_table (Nodes, Model) ->
+  Record = boss_record_lib:dummy_record (Model),
+  { Model, create_table (Nodes, Model, Record:attribute_names ()) }.
+
+% single table creator wrapper
+create_table (Nodes, Table, Attribs) ->
+  mnesia:create_table (Table,
+    [ { disc_copies, Nodes   },
+      { attributes,  Attribs } ]).
 
 migrate() ->
     mnesia:stop(),
@@ -35,7 +49,3 @@ rebuild() ->
     mnesia:stop(),
     mnesia:delete_schema(?NODES),
     init().
-
-recreate_table(Model) ->
-    mnesia:delete_table(Model),
-    create_table(Model).
