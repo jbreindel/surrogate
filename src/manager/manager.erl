d58fbedd0a68d0502ff44546416e13cdf1b3cc06
Jake Breindel <j.breindel2@outlook.com>
2015-05-26 13:15:10 +0000

    manager.erl created online with Bitbucket

---
+++ src/manager/manager.erl
@@ -0,0 +1,13 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% FILE: manager.erl
+%
+% AUTHOR: Jake Breindel
+% DATE: 5-26-15
+%
+% DESCRIPTION:
+%
+% Manager class that runs in seperate
+% process and manages download events.
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
