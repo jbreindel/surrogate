fbdf9fc28c4c4d3448f0ada21e33363a9e7b1593
Jake Breindel <j.breindel2@outlook.com>
2015-06-01 23:11:59 -0400

    Updated manager

--- src/manager/manager.erl
+++ src/manager/manager.erl
@@ -18,10 +18,21 @@
 loop(Account) ->
 	loop(Account, [], undefined);
 
-loop(Account, ActiveDownloads, WebSocket) ->
+loop(Account, ActiveDownloads, Websocket) ->
 	recieve
+
+		%%%%%%%%%%%%%%%%%%%%%
+		%% Client Messages %%
+		%%%%%%%%%%%%%%%%%%%%%
+
+		{ws_connect, WebSocket} ->
+			loop(Account, ActiveDownloads, Websocket);
+
 		{links, Links} ->
 			handle_links(Account, Links);
+
+		{ws_disconnect, _} ->
+			loop(Account, ActiveDownloads, undefined).
 
 %% handle_links(Account, Links) ->
 %% 	case download_lib:save_downloads(Account:premium(), Links, []) of
