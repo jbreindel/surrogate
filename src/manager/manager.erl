1bdaa37d2c902a5ef7a9c03f0d8537c4fdc707db
Jake Breindel <j.breindel2@outlook.com>
2015-07-15 17:16:00 +0000

    Added more messages to be received in the manager.

--- src/manager/manager.erl
+++ src/manager/manager.erl
@@ -15,9 +15,19 @@
 -module(manager).
 -export(loop/1).
 
+%%----------------------------------------------------------------------
+%% Function: loop/1
+%% Purpose: Loops the manager with an Account and default options
+%% Args:   Req - Account
+%%----------------------------------------------------------------------
 loop(Account) ->
-	loop(Account, [], undefined, undefined);
+	loop(Account, [], undefined);
 
+%%----------------------------------------------------------------------
+%% Function: loop/3
+%% Purpose: Loops the manager with an Account, Downloads, and Subscriber
+%% Args:   Req - Account
+%%----------------------------------------------------------------------
 loop(Account, Downloads, Subscriber) ->
 	recieve
 
@@ -25,15 +35,45 @@ loop(Account, Downloads, Subscriber) ->
 		%% Client Messages %%
 		%%%%%%%%%%%%%%%%%%%%%
 
+		%%
+		% subscriber connects to manager
+		%%
 		{subscriber_connect, Subscriber} ->
+			Subscriber ! {downloads Downloads},
 			loop(Account, Downloads, Subscriber);
 
+		%%
+		% subscriber sent links to the manager
+		%%
 		{links, Links} ->
 			handle_links(Account, Links),
-			loop(Account, Downloads, Subscriber)
+			loop(Account, Downloads, Subscriber);
 
+		%%
+		% subscriber no longer is connected
+		%%
 		{subscriber_disconnect, _} ->
-			loop(Account, Downloads, undefined).
+			loop(Account, Downloads, undefined);
+			
+		%%%%%%%%%%%%%%%%%%%%%%%
+		%% Download Messages %%
+		%%%%%%%%%%%%%%%%%%%%%%%
+		
+		%%
+		% download has been accquired
+		%%
+		{download_accquired, Download} ->
+			case is_pid(Subscriber) of
+				true ->
+					UpdatedDownload = Download::set(status, ?DL_PENDING),
+					case UpdatedDownload::save() of
+						{ok, SavedDownload} ->
+            				Subscriber ! {on_download_accquired, Download};
+						{error, Errors} ->
+				   			{ok, [{errors, Errors}, {config, Config}]}
+					end;
+			end
+	end.
 
 %% handle_links(Account, Links) ->
 %% 	case download_lib:save_downloads(Account:premium(), Links, []) of
