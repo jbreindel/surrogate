5e0f89bbf68fc0d290671380e6d3664130a2e5b3
Jake Breindel <j.breindel2@outlook.com>
2015-07-20 16:28:21 +0000

    premium.erl edited online with Bitbucket

--- src/model/premium.erl
+++ src/model/premium.erl
@@ -11,7 +11,7 @@
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
--module(premium, [Id, UserName::string(), Password::string(), AccountId::integer()]).
+-module(premium, [Id, Type::string(), UserName::string(), Password::string(), AccountId::integer()]).
 -compile(export_all).
 -belongs_to(account).
 -has({downloads, many}).
