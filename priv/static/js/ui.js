094a0fe2e105e16068144fb9caddcb977a3aac08
Jake Breindel <j.breindel2@outlook.com>
2015-10-31 12:45:16 -0400

    Added code for websockets.

--- priv/static/js/ui.js
+++ priv/static/js/ui.js
@@ -14,6 +14,29 @@
  
  	'use strict';
  	
+ 	// open a websocket
+ 	var managerSocket = new WebSocket('ws://' + location.host + '/');
+ 	
+ 	// called when socket opens
+ 	managerSocket.onopen = function(e) {
+ 		
+ 	};
+ 	
+ 	// called when an error occurs with the socket
+ 	manager.onerror = function(e) {
+ 		
+ 	};
+ 	
+ 	// called when the socket closes
+ 	managerSocket.onclose = function(e) {
+ 		
+ 	};
+ 	
+ 	// called when we receive a message
+ 	manager.onmessage = function(e) {
+ 		
+ 	};
+ 	
  	// adds new downloads
  	$('#add-downloads').click(function(e) {
  	
--- src/websocket/surrogate_manager_websocket.erl
+++ src/websocket/surrogate_manager_websocket.erl
@@ -16,3 +16,21 @@
 
 init() -> 
 	{ok, []}.
+
+handle_join(ServiceURL, WebSocket, State) -> 
+	{noreply, []}.
+
+handle_incoming(ServiceURL, WebSocket, Message, State) ->
+	{noreply, []}.
+
+handle_broadcast(Message, State) ->
+	{noreply, []}.
+
+handle_info(Info, State) ->
+	{noreply, NewState}.
+
+handle_close(Reason, ServiceURL, WebSocket, State) ->
+	{noreply, []}.
+
+terminate(Reason, State) -> 
+	ok. 
