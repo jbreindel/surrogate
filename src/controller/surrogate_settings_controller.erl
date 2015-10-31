caab09157738307a6464891f280a21e87618ffbb
Jake Breindel <j.breindel2@outlook.com>
2015-10-31 02:13:55 -0400

    Added code for premium login along with a settings page.

---
+++ src/controller/surrogate_settings_controller.erl
@@ -0,0 +1,36 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% FILE: surrogate_settings_controller.erl
+%
+% AUTHOR: Jake Breindel
+% DATE: 10-30-15
+%
+% DESCRIPTION:
+%
+% Controller for settings form.
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+-module(surrogate_settings_controller, [Req]).
+-compile(export_all).
+
+before_(_) ->
+    account_lib:require_login(Req).
+
+settings('GET', [], Account) ->
+	{ok, [{account, Account}]};
+
+settings('POST', [], Account) ->
+	Premium = premium:new(id, Req:post_param("email"), Req:post_param("password"), Account:id()),
+	case premium_lib:premium_login(Premium) of
+		true ->
+			case Premium:save() of
+				{ok, SavedPremium} ->
+					%% TODO add to flashbag
+					{redirect, "/settings/settings"};
+		        {error, Error} ->
+					{ok, [{error, Error}]}
+			end;
+		false ->
+			{ok, [{error, [{emailError, "Unable to login to account"}]}]}
+	end.
---
+++ src/lib/premium_lib.erl
@@ -0,0 +1,43 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% FILE: premium_lib.erl
+%
+% AUTHOR: Jake Breindel
+% DATE: 10-31-15
+%
+% DESCRIPTION:
+%
+% Library for premium functions.
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+-module(form_lib).
+-export([premium_login/1]).
+
+rapidgator_login_form(Email, Password) ->
+	UrlEmail = edoc_lib:escape_uri(Email),
+	UrlPassword = edoc_lib:escape_uri(Password),
+	"LoginForm%5Bemail%5D=" ++ UrlEmail ++ 
+	"&LoginForm%5Bpassword%5D=" ++ UrlPassword ++ 
+	"&LoginForm%5BrememberMe%5D=0&LoginForm%5BrememberMe%5D=1&LoginForm%5BverifyCode%5D=".
+		
+
+rapidgator_login(Premium) ->
+	Form = rapidgator_login_form(Premium:userName(), Premium:password()),
+	case httpc:request(post, {"https://rapidgator.net/auth/login", [], 
+							  "application/x-www-form-urlencoded", Form}, [], []) of
+		{ok, {{Version, 302, ReasonPhrase}, Headers, Body}} ->
+			true;
+		true ->
+			false
+	end.
+	
+
+premium_login(Premium) ->
+	case Premium:type() of
+		"Rapidgator" ->
+			rapidgator_login(Premium);
+		_ ->
+			false
+	end.
+			
