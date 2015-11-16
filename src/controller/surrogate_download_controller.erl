032ca79686339d25d142a0ef9be18227aa618c4e
Jake Breindel <j.breindel2@outlook.com>
2015-11-15 21:33:38 -0500

    Added code for download status type filters.

--- src/controller/surrogate_download_controller.erl
+++ src/controller/surrogate_download_controller.erl
@@ -20,21 +20,21 @@ before_(_) ->
 
 downloads('GET', [], Account) ->
 	Premium = Account:premium(),
-	case Req:query_param("filter", "pending") of
+	case Req:query_param("status", "pending") of
 		"pending" ->
 			Downloads = boss_db:find(download, 
 									 [{status, in, [?DL_PENDING, ?DL_ACQUIRED, ?DL_ACTIVE, ?DL_PAUSED]}], 
 									 [{order_by, status}, {descending, true}]),
-			{ok, [{downloads, Downloads}]};
+			{ok, [{status_type, "pending"}, {downloads, Downloads}]};
 		"completed" ->
 			Downloads = boss_db:find(download, 
 									 [{status, equals, ?DL_COMPLETED}], 
 									 [{order_by, status}, {descending, true}]),
-			{ok, [{downloads, Downloads}]};
+			{ok, [{status_type, "completed"}, {downloads, Downloads}]};
 		"failed" ->
 			Downloads = boss_db:find(download, 
 									 [{status, in, [?DL_FAILED, ?DL_NOT_FOUND]}], 
 									 [{order_by, status}, {descending, true}]),
-			{ok, [{downloads, Downloads}]}
+			{ok, [{status_type, "failed"}, {downloads, Downloads}]}
 	end.
 
